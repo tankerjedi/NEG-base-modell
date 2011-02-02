855cd36c3b93bac467dc22c6262cc090bef179ad
tankerjedi <tankerjedi@gmail.com>
2011-02-02 16:14:56 +0100

    Modell works in a simple situation

--- base_modell.cpp
+++ base_modell.cpp
@@ -7,6 +7,8 @@ int main()
 {
 printf("Simulation starts\n");
 
+printf("Wage in region1 is: %4.2f\n",wage_region1( income_region1(1), income_region2(1), price_index_region1(1,1), price_index_region2(1,1)));
+
 return 0;
 }
 
@@ -15,54 +17,54 @@ return 0;
 
 //Base modell
 
-float income_region1(float wage_region1)
+float income_region1(float wage1)
 {
-return mu * lambda * wage_region1 + (1 - mu) / 2;
+return mu * lambda * wage1 + (1 - mu) / 2;
 };
 
-float income_region2(float wage_region2)
+float income_region2(float wage2)
 {
-return mu * (1 - lambda) *wage_region2 + (1 - mu) / 2;
+return mu * (1 - lambda) *wage2 + (1 - mu) / 2;
 };
 
-float price_index_region1(float wage_region1, float wage_region2)
+float price_index_region1(float wage1, float wage2)
 {
-return pow(lambda * pow (wage_region1, 1 - sigma) + (1 - lambda) * pow (wage_region2 * T, 1 - sigma),(1 / (1 - sigma)));
+return pow(lambda * pow (wage1, 1 - sigma) + (1 - lambda) * pow (wage2 * T, 1 - sigma),(1 / (1 - sigma)));
 };
 
-float price_index_region2(float wage_region1, float wage_region2)
+float price_index_region2(float wage1, float wage2)
 {
-return pow(lambda * pow (wage_region1 * T, 1 - sigma) + (1 - lambda) * pow (wage_region2, 1 - sigma),(1 / (1 - sigma)));
+return pow(lambda * pow (wage1 * T, 1 - sigma) + (1 - lambda) * pow (wage2, 1 - sigma),(1 / (1 - sigma)));
 };
 
-float wage_region1(float income1, float income2, float price_index_region1, float price_index_region2)
+float wage_region1(float income1, float income2, float price_index1, float price_index2)
 {
-return pow(income1 * pow (price_index_region1, sigma - 1) + income2 * pow (price_index_region2 / T, sigma -1),(1 /  sigma));
+return pow(income1 * pow (price_index1, sigma - 1) + income2 * pow (price_index2 / T, sigma -1),(1 /  sigma));
 };
 
-float wage_region2(float income1, float income2, float price_index_region1, float price_index_region2)
+float wage_region2(float income1, float income2, float price_index1, float price_index2)
 { 
-return pow(income1 * pow (price_index_region1 / T, sigma - 1) + income2 * pow (price_index_region2 , sigma -1),(1 /  sigma));
+return pow(income1 * pow (price_index1 / T, sigma - 1) + income2 * pow (price_index2 , sigma -1),(1 /  sigma));
 };
 
-float real_wage_region1(float wage_region1, float price_index_region1)
+float real_wage_region1(float wage1, float price_index1)
 {
-return wage_region1 * pow(price_index_region1, -mu);
+return wage1 * pow(price_index1, -mu);
 };
 
-float real_wage_region2(float wage_region2, float price_index_region2)
+float real_wage_region2(float wage2, float price_index2)
 {
-return wage_region2 * pow(price_index_region2, -mu);
+return wage2 * pow(price_index2, -mu);
 };
 
 //Dinamics
 
-float average_real_wage(float real_wage_region1, float real_wage_region2)
+float average_real_wage(float real_wage1, float real_wage2)
 {
 return 0;
 };
 
-float new_lamda(float average_real_wage, float real_wage_region1) //The value of the population share in the next
+float new_lamda(float average_real_wage0, float real_wage1) //The value of the population share in the next
 {
 return 0;
 
