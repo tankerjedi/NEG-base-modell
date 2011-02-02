7bb1a1207795c5c762579634ce3aa43c1d0c2876
tankerjedi <tankerjedi@gmail.com>
2011-02-02 15:39:57 +0100

    Modifications

--- base_modell.cpp
+++ base_modell.cpp
@@ -1,10 +1,68 @@
 #include <stdio.h>
 
-//#inculde "base_modell.h"
+#include "base_modell.h"
 
 int main()
 {
-printf("Csumi\n");
+printf("Simulation starts\n");
 
 return 0;
 }
+
+//Equations
+
+
+//Base modell
+
+float income_region1(float wage_region1)
+{
+return 0;
+};
+
+float income_region2(float wage_region2)
+{
+return 0;
+};
+
+float price_index(g_region1(float wage_region1, float wage_region2)
+{
+return 0;
+};
+
+float price_index(g_region2(float wage_region1, float wage_region2)
+{
+return 0;
+};
+
+float wage_region1(float price_index_region1, float price_index_region2)
+{
+return 0;
+};
+
+float wage_region2(float price_index_region1, float price_index_region2)
+{ 
+return 0;
+};
+
+float real_wage_region1(float wage_region1, float price_index_region1)
+{
+return 0;
+};
+
+float real_wage_region2(float wage_region2, float price_index_region2)
+{
+return 0;
+};
+
+//Dinamics
+
+float average_real_wage(float real_wage_region1, float real_wage_region2)
+{
+return 0;
+};
+
+float new_lamda(float average_real_wage, float real_wage_region1) //The value of the population share in the next
+{
+return 0;
+
+};
--- base_modell.h
+++ base_modell.h
@@ -9,30 +9,32 @@ float gamma =1; //Speed of population moving
 float  mu = 0.5; //Cobb-Douglas utility function parameter
 float sigma = 1; //CES utility fucnion parameter
 
+float T = 1.2; //Transportation cost
+
 //Equations
 
 
 //Base modell
 
-float income_region1(float wage_region1);
+float income_region1(float wage1);
 
-float income_region2(float wage_region2);
+float income_region2(float wage2);
 
-float price_index(g_region1(float wage_region1, float wage_region2);
+float price_index_region1(float wage1, float wage2);
 
-float price_index(g_region2(float wage_region1, float wage_region2);
+float price_index_region2(float wage1, float wage2);
 
-float wage_region1(float price_index_region1, float price_index_region2);
+float wage_region1(float price_index1, float price_index2);
 
-float wage_region2(float price_index_region1, float price_index_region2);
+float wage_region2(float price_index1, float price_index2);
 
-float real_wage_region1(float wage_region1, float price_index_region1);
+float real_wage_region1(float wage1, float price_index1);
 
-float real_wage_region2(float wage_region2, float price_index_region2);
+float real_wage_region2(float wage2, float price_index2);
 
 //Dinamics
 
-float average_real_wage(float real_wage_region1, float real_wage_region2);
+float average_real_wage(float real_wage1, float real_wage2);
 
-float new_lamda(float average_real_wage, float real_wage_region1); //The value of the population share in the next 
+float new_lamda(float average_real_wage0, float real_wage1); //The value of the population share in the next 
 
