54ff471a8bef9560646353cc5135566d21dbc04f
tankerjedi <tankerjedi@gmail.com>
2011-02-02 15:11:45 +0100

    Parameters and new equations in header file

--- base_modell.h
+++ base_modell.h
@@ -1,6 +1,19 @@
 
 //Parameters
 
+int iteration_limit = 500; //Itertaion limit
+
+float lambda = 0.5; //Region's population share in region1 parameter
+float gamma =1; //Speed of population moving 
+
+float  mu = 0.5; //Cobb-Douglas utility function parameter
+float sigma = 1; //CES utility fucnion parameter
+
+//Equations
+
+
+//Base modell
+
 float income_region1(float wage_region1);
 
 float income_region2(float wage_region2);
@@ -17,5 +30,9 @@ float real_wage_region1(float wage_region1, float price_index_region1);
 
 float real_wage_region2(float wage_region2, float price_index_region2);
 
+//Dinamics
 
+float average_real_wage(float real_wage_region1, float real_wage_region2);
+
+float new_lamda(float average_real_wage, float real_wage_region1); //The value of the population share in the next 
 
