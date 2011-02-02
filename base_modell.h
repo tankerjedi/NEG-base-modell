054b4facc0644daadfb2b0a430464114e69520b9
tankerjedi <tankerjedi@gmail.com>
2011-02-02 14:32:27 +0100

    Header file

---
+++ base_modell.h
@@ -0,0 +1,21 @@
+
+//Parameters
+
+float income_region1(float wage_region1);
+
+float income_region2(float wage_region2);
+
+float price_index(g_region1(float wage_region1, float wage_region2);
+
+float price_index(g_region2(float wage_region1, float wage_region2);
+
+float wage_region1(float price_index_region1, float price_index_region2);
+
+float wage_region2(float price_index_region1, float price_index_region2);
+
+float real_wage_region1(float wage_region1, float price_index_region1);
+
+float real_wage_region2(float wage_region2, float price_index_region2);
+
+
+
