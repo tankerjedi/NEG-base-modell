0f9a30b8f626d9381ea87812110d993894943da4
tankerjedi <tankerjedi@gmail.com>
2011-03-26 11:44:46 +0100

    dprice_index_region1

--- base_modell.cpp
+++ base_modell.cpp
@@ -15,6 +15,8 @@ printf("Value of dL/dw1: %4.2f\n", dwage_region1(1.1,1,price_index_region1(1.1,1
 
 printf("Value of dL/dw2: %4.2f\n", dwage_region2(1,1,price_index_region1(1,1),price_index_region2(1,1)));
 
+printf("Value of dL/dG1: %4.2f\n", dprice_index_region1(1,1,price_index_region1(1,1),price_index_region2(1,1)));
+
 return 0;
 }
 
@@ -113,7 +115,12 @@ return
 
 float dprice_index_region1(float wage1, float wage2, float price_index1, float price_index2)
 {
-return 0;
+
+return
+-2 * ( price_index_region1(wage1,wage2)- price_index1) + 
+2 * (wage_region1(income_region1(wage1),income_region2(wage2),price_index1,price_index2) - wage1) * ( 1 / sigma) *  pow(wage_region1(income_region1(wage1),income_region2(wage2),price_index1,price_index2), 1 - sigma) * (mu * (1 - lambda) * wage1 + (1 - mu) / 2 ) * (sigma - 1) * pow ( price_index1 , sigma - 2) +
+2 * (wage_region2(income_region1(wage1),income_region2(wage2),price_index1,price_index2) - wage2) *  ( 1 / sigma) *  pow(wage_region2(income_region1(wage1),income_region2(wage2),price_index1,price_index2), 1 - sigma) * (mu * lambda * wage1 + (1 - mu) / 2 ) * (sigma - 1) * pow(T , 1 - sigma) * pow ( price_index1 , sigma - 2) ;
+
 }
 ;
 
